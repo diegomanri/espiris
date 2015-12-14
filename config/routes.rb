7dca271e74aca9c2e131c2a4b97fe8ba0f97a7ed
Diego A. Manrique Parraga <diegomanri@gmail.com>
2015-12-14 13:47:24 -0800

    Adding a root page and setting up for public static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  get "/public_pages/:page" => "public_pages#show"
+  root "public_pages#show", page: "index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
